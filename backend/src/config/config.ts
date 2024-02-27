38719586eb3e1dd38fd342104a73e7a678e0dc8f
R2u1s <Budz@yandex.ru>
2024-02-28 01:02:39 +0300

    поправить все косяки

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -21,11 +21,11 @@ export default () => ({
     port: parseInt(process.env.PORT, 10) || 3001
   },
   database: {
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST || 'postgres',
     port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-    username: process.env.POSTGRES_USERNAME || 'student',
+    username: process.env.POSTGRES_USER || 'student',
     password: process.env.POSTGRES_PASSWORD || 'student',
-    name: process.env.POSTGRES_NAME || 'kupipodariday'
+    name: process.env.POSTGRES_DB || 'kupipodariday'
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'jwt_secret'
